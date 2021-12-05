038cc6b660247140978a7f80e6121e54095a6509
Chanuka Dinuwan <chanukadinuwan35@gmail.com>
2021-12-06 00:27:12 +0530

    Added

---
+++ MochaHooks.js
@@ -0,0 +1,18 @@
+describe('Mocha Hooks',function(){
+
+    before('Execute Before All Tests',function(){
+        console.log('Execute Before All Tests');
+    });
+
+    beforeEach('Execute Before Each Tests',function(){
+        console.log('Execute Before Each Test');
+    });
+
+    after('Execute After All Tests',function(){
+        console.log('Execute After All Tests');
+    });
+
+    before('Execute After Each Tests',function(){
+        console.log('Execute After Each Test');
+    });
+});
